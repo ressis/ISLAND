b1610e5bb51e6df3d1deb64b74bbac073d357305
Anastasia <anastasia.ressis@gmail.com>
2010-01-16 00:08:17 -0600

    Added the browser object. Beginning the installed browser detection story.

---
+++ ISLAND/ISLAND Management Console/Browser.vb
@@ -0,0 +1,15 @@
+﻿Public Class Browser
+	Private id As Nullable(Of Int64) = Nothing
+	Private name As String = Nothing
+	Private command As String = Nothing
+
+	Public Sub New(ByVal _id As Int64, ByVal _name As String, ByVal _command As String)
+		id = _id
+		name = _name
+		command = _command
+	End Sub
+
+	Public Function IsComplete() As Boolean
+		Return (id IsNot Nothing AndAlso name IsNot Nothing AndAlso command IsNot Nothing)
+	End Function
+End Class
--- ISLAND/ISLAND Management Console/DatabaseHelpers.vb
+++ ISLAND/ISLAND Management Console/DatabaseHelpers.vb
@@ -1,14 +1,22 @@
 ﻿Public Class DatabaseHelpers
 	Private Shared conn_string As String = ""
 	Private Shared conn As System.Data.SQLite.SQLiteConnection
+	Private Shared newDB As Boolean = False
 
 	Private Shared ReadOnly Property ConnectionString() As String
 		Get
 			If conn_string.Equals(String.Empty) Then
+				Dim db_file As String = System.IO.Path.Combine(System.Environment.GetEnvironmentVariable("APPDATA"), "ISLAND\urls.sqlite3")
 				Dim conn_string_builder As New System.Data.SQLite.SQLiteConnectionStringBuilder()
-				conn_string_builder.DataSource = System.IO.Path.Combine(System.Environment.GetEnvironmentVariable("APPDATA"), "ISLAND\urls.sqlite3")
+				conn_string_builder.DataSource = db_file
 
 				conn_string = conn_string_builder.ToString
+
+				If Not System.IO.File.Exists(db_file) Then
+					newDB = True
+					System.IO.Directory.CreateDirectory(System.IO.Path.GetDirectoryName(db_file))
+					System.Data.SQLite.SQLiteConnection.CreateFile(db_file)
+				End If
 			End If
 			Return conn_string
 		End Get
@@ -34,6 +42,9 @@
 	End Sub
 
 	Private Shared Sub Initialize_Database(ByVal conn As System.Data.SQLite.SQLiteConnection)
+		If Not newDB Then
+			Return
+		End If
 		Dim connectionState As ConnectionState = conn.State
 
 		Dim browserTable As System.Data.SQLite.SQLiteCommand = conn.CreateCommand
@@ -52,5 +63,9 @@
 		If connectionState = Data.ConnectionState.Closed Then
 			conn.Close()
 		End If
+	End Sub
+
+	Private Shared Sub Discover_Browsers()
+
 	End Sub
 End Class
